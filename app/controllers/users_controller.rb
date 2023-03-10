d342f2e454a411d08baa28d31666a339b50a0bde
Maia <maia@Maias-MacBook-Air.local>
2023-03-10 22:32:44 +0800

    generate user model using scaffold

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,64 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user, except: %i[new create]
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      flash[:notice] = "Welcome, #{@user.first_name}"
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Account updated."
+      redirect_to user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+   if current_user.admin?
+    @user.destroy
+    flash[:notice] = "User deleted."
+    redirect_to users_path
+  else
+    log_out
+    @user.destroy
+    flash[:notice] = "Account deleted."
+    redirect_to root_url
+   end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home_page#index"
+
+  get "home_page/index"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  resources :users
 end
