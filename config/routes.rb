563768e388c8aa638cf023315433ddff29ee4dcd
Maia <maia@Maias-MacBook-Air.local>
2023-03-20 23:33:13 +0800

    fix routes to include bid

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
   root "home_page#index"
-  
+
   get "home_page/index"
-  
+
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  
-  resources :bids
-  resources :products
-  resources :users
+
+  resources :users do
+    resources :bids, only: %i[index]
+  end
+  resources :products do
+    resources :bids, only: %i[new create edit update]
+  end
 end
