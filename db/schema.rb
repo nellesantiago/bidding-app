ab3cc1ce79b007f6d459e91e1030a42090309a66
Maia <maia@Maias-MacBook-Air.local>
2023-03-20 14:32:00 +0800

    create and migrate bid table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_053421) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_18_162754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bids", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_bids_on_product_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
@@ -36,5 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_053421) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bids", "products"
+  add_foreign_key "bids", "users"
   add_foreign_key "products", "users"
 end
