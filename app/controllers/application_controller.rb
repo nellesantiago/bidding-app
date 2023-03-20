fd1e5f34de26d0a9df3d6048629e3d052da5f05f
Maia <maia@Maias-MacBook-Air.local>
2023-03-20 23:39:12 +0800

    check bidding status logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper
+  include SessionsHelper
+  before_action :check_expiry
+
+  def check_expiry
+    @products_active = Product.where(bidding_status: "active")
+    @products_active.each do |product|
+      if product.bidding_expiration.past?
+        product.update!(bidding_status: "expired")
+    end
+    end
+  end
 end
